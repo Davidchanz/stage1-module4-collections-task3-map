657d15811e24b72872d1fffe950bc7c3c5c8d54c
Davidchanz <tovliskatsi@gmail.com>
2023-07-25 17:37:59 +0400

    Collection Task 3 Map completed

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        sentence = sentence.replaceAll(",", "");
+        sentence = sentence.replaceAll("\\.", "");
+        for(var word: sentence.toLowerCase().split(" ")){
+            if(!word.isBlank()) {
+                if (result.containsKey(word)) {
+                    result.put(word, result.get(word) + 1);
+                } else {
+                    result.put(word, 1);
+                }
+            }
+        }
+        return result;
     }
 }
